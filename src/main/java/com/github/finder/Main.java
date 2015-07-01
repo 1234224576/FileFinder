99bd004d8f26c3c2298562b521db697e0da79032
曽和修平 <sowashuhei@gmail.com>
2015-07-01 18:13:53 +0900

    コマンドラインオプションを解析する処理を実装

--- src/main/java/com/github/finder/Main.java
+++ src/main/java/com/github/finder/Main.java
@@ -2,9 +2,19 @@ package com.github.finder;
 
 public class Main{
     public Main(String[] arguments){
+      Args args = parseArguments(arguments);
     }
 
     public static void main(String[] args){
         new Main(args);
+    }
+    private Args parseArguments(String[] arguments){
+        Args args = new Args();
+        try {
+            CmdLineParser parser = new CmdLineParser(args);
+            parser.parseArgument(arguments);
+        } catch (CmdLineException e) {
+        }
+        return args;
     }
 }
